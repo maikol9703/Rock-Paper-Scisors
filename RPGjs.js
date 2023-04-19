8e6e606553efe7c26944baf5f119a3bf6e1b99f7
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-19 17:28:33 -0600

    Create the loop and conditionals for each round
    Conditionals will track the scores to find a winner

--- RPGjs.js
+++ RPGjs.js
@@ -15,6 +15,39 @@ function game(){
     let count=0;
     let scoreComp=0;
     let scoreUser=0;
+
+    while (count<= 5){
+        count++;
+        let plays=playerSelection(prompt("choose your arm: "));
+        let compTurnn=compTurn();
+        if (compTurnn===plays){
+            console.log("its a tie bro")
+        } else if (compTurnn==="Rock" && plays==="Scissors"){
+            console.log("you lost");
+            scoreComp+=1;
+        } else if (plays==="Rock" && compTurnn==="Scissors"){
+            console.log("you win");
+            scoreUser+=1;
+        } else if (compTurnn==="Rock" && plays==="Paper"){
+            console.log("You win");
+            scoreUser+=1;
+        } else if (plays==="Rock" && compTurnn==="Paper"){
+            console.log("You lost");
+            scoreComp+=1;
+        } else if (plays==="Scissors" && compTurnn==="Paper"){
+            console.log("You win");
+            scoreUser+=1;
+        }else if (plays==="Paper" && compTurnn==="Scissors"){
+            console.log("You lost");
+            scoreComp+=1;
+        } else {
+            console.log("error en la matrix")
+        };
+        
+        console.log(`The current score is: \ncomputer: ${scoreComp}\nyou: ${scoreUser}`)
+   
+    };
+    
     if (scoreComp>scoreUser){
         console.log("Uh oh, you lost ma boi")
     } else {
