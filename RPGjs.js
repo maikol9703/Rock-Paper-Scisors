563ca1c11959b7829f731700ae0e59b3f310b5ac
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-19 17:26:52 -0600

    Create a function to create all variables used
    Create messages as well for the game finale

--- RPGjs.js
+++ RPGjs.js
@@ -10,3 +10,17 @@ function playerSelection(playsel){
     let plays=playsel.charAt(0).toUpperCase() + playsel.slice(1).toLowerCase();
     return (plays);
 }
+
+function game(){
+    let count=0;
+    let scoreComp=0;
+    let scoreUser=0;
+    if (scoreComp>scoreUser){
+        console.log("Uh oh, you lost ma boi")
+    } else {
+        console.log("ohh sweet, you won ma son")
+    }   
+    
+}
+
+game();
